66f179ae633ffe02fc8d6630a9c9c2031a23971c
Glasaqui <gsanchezquiroz@outlook.com>
2022-09-13 23:27:58 +0000

    Generador de cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cartas = ["♦", "♥", "♠", "♣"];
+  // let cartas = ["diamond", "heart"];
+  let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let UnaCarta = cartas[Math.floor(Math.random() * cartas.length)];
+  let UnNumero = numeros[Math.floor(Math.random() * numeros.length)];
+  let VerCarta = `${UnaCarta}`;
+  //let VerCarta = `${UnaCarta} ${UnNumero}`;
+  document.querySelector(".top-card").innerHTML = UnaCarta;
+  document.querySelector(".numero").innerHTML = UnNumero;
+  document.querySelector(".foot-card").innerHTML = UnaCarta;
 };
